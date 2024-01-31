a4650008a5a805c2da9c3adda78979eb456c56a3
Mikkel RINGAUD <mikkel@milescode.dev>
2024-02-01 00:41:08 +0100

    feat: add entry points

---
+++ src/classes/FortiGateWebSSLVPN.ts
@@ -0,0 +1,9 @@
+class FortiGateWebSSLVPN {
+  constructor (
+    private proxyID: string,
+    private token: string,
+    private origin: string
+  ) {}
+}
+
+export default FortiGateWebSSLVPN;
---
+++ src/methods/initWebSSLVPNSession.ts
@@ -0,0 +1,19 @@
+import { createLogin } from "./createLogin";
+import { transformTemporaryToken } from "./transformTemporaryToken";
+import FortiGateWebSSLVPN from "../classes/FortiGateWebSSLVPN";
+import { readProxyID } from "./readProxyID";
+
+/**
+ * Entry point to create a new `FortiGateWebSSLVPN` instance.
+ * This method will authenticate the user on the provided origin.
+ */
+export const initWebSSLVPNSession = async (username: string, password: string, origin: string): Promise<FortiGateWebSSLVPN> => {
+  // Remove trailing slash from origin, if exists.
+  origin = origin.replace(/\/$/, "");
+  const { redirectionPath, tempToken } = await createLogin(username, password, origin);
+
+  const token = await transformTemporaryToken(tempToken, origin + redirectionPath);
+  const proxyID = await readProxyID(token, origin);
+
+  return new FortiGateWebSSLVPN(proxyID, token, origin);
+};
